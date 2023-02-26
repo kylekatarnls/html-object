b21e48c5e4f59500fbb511dc9c5e7a88d25b1018
KyleKatarn <kylekatarnls@gmail.com>
2023-02-26 11:58:17 +0100

    Use getter for unit test

--- tests/TagTest.php
+++ tests/TagTest.php
@@ -224,14 +224,14 @@ class TagTest extends HtmlObjectTestCase
 
         $this->assertEquals('<p class="bar">foo</p>', $this->getObject()->render());
     }
-        
+
     public function testCannotRemoveWrongClasses()
     {
-        $this->object->addClass('foo');
-        $this->object->addClass('bar');
-        $this->object->removeClass('unknow');
+        $this->getObject()->addClass('foo');
+        $this->getObject()->addClass('bar');
+        $this->getObject()->removeClass('unknow');
 
-        $this->assertEquals('<p class="foo bar">foo</p>', $this->object->render());
+        $this->assertEquals('<p class="foo bar">foo</p>', $this->getObject()->render());
     }
 
     public function testCanManuallyOpenElement()
